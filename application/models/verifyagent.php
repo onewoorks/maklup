bc4ee09cc9347200991e4e17d12d321e05e47110
onewoorks <irwanbibrahim@onewoorks.com>
2019-04-09 11:39:03 +0800

    create table

--- application/models/verifyagent.php
+++ application/models/verifyagent.php
@@ -15,22 +15,32 @@ class VerifyAgent_Model extends Common_Model {
     }
 
     private function CreateTable() {
-        
+        $query = "CREATE TABLE `verify_agent` (`id` int(11) unsigned "
+                . "NOT NULL AUTO_INCREMENT, "
+                . "`timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP, "
+                . "`pemohon_id` int(11) NOT NULL, "
+                . "`verify_status` varchar(20) NOT NULL DEFAULT '', "
+                . "`staff_id` int(11) NOT NULL, "
+                . "`device_id` varchar(100) DEFAULT NULL, "
+                . "PRIMARY KEY (`id`)) "
+                . "ENGINE=InnoDB AUTO_INCREMENT=2 "
+                . "DEFAULT CHARSET=utf8";
+        $this->db->executeQuery($query);
     }
-    
-    public function CreateNewData($data){
+
+    public function CreateNewData($data) {
         $query = "INSERT INTO $this->verify_agent "
                 . "(pemohon_id, verify_status, staff_id,device_id) "
                 . "VALUE ("
-                . "'". (int) $data['pemohon_id']."', "
-                . "'". $this->db->escape($data['verify_status'])."', "
-                . "'".(int) $data['staff_id']."', "
-                . "'".$this->db->escape($data['device_id'])."')";
+                . "'" . (int) $data['pemohon_id'] . "', "
+                . "'" . $this->db->escape($data['verify_status']) . "', "
+                . "'" . (int) $data['staff_id'] . "', "
+                . "'" . $this->db->escape($data['device_id']) . "')";
         echo $query;
         $this->db->executeQuery($query);
     }
-    
-    public function ReadVerifyData(){
+
+    public function ReadVerifyData() {
         $query = "SELECT v.timestamp as tarikh_verify, "
                 . "p.data_pemohon as data_pemohon,"
                 . "v.verify_status as status "
